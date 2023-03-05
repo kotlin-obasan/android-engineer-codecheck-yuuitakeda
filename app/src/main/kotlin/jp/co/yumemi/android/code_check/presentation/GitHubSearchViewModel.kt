3a4f23a73f9375b166ad869738de089400bc2272
YuuiTakeda <yuuitakeda@gmail.com>
2023-03-05 18:33:09 +0900

    ViewModelに対し依存関係注入可能にする

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/GitHubSearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/GitHubSearchViewModel.kt
@@ -1,15 +1,17 @@
 /*
  * Copyright © 2021 YUMEMI Inc. All rights reserved.
  */
-package jp.co.yumemi.android.code_check
+package jp.co.yumemi.android.code_check.presentation
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.data.dto.GitHubRepositoryInfo
 import jp.co.yumemi.android.code_check.presentation.TopActivity.Companion.lastSearchDate
 import kotlinx.coroutines.GlobalScope
@@ -22,6 +24,7 @@ import java.util.*
  * GitHubSearchViewModel
  * GitHubSearchFragmentに描画する情報を保持するViewModel
  */
+@HiltViewModel
 class GitHubSearchViewModel(
     val context: Context
 ) : ViewModel() {
